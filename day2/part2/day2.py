580c7e319dbd4507517086ae89f51d9137d6a47d
pstreff <streff@convidera.com>
2019-12-03 13:44:34 +0100

    part2 day2

--- day2/part2/day2.py
+++ day2/part2/day2.py
@@ -1,33 +1,32 @@
 
 def main():
-    reader = open('input.txt', 'r')
-    input_list = list(map(int, reader.read().split(',')))
-    pointer = 0
+    for noun in range(0, 100):
+        for verb in range(0, 100):
+            input_list, pointer = reset_memory()
 
-    restore_1202_program_alarm_state(input_list)
+            input_list[1] = noun
+            input_list[2] = verb
 
-    print(input_list)
+            while input_list[pointer] != 99:
+                opcode, first_input, output_position, second_input = get_instructions(input_list, pointer)
 
-    while input_list[pointer] != 99:
-        opcode, first_input, output_position, second_input = get_instructions(input_list, pointer)
+                if opcode == 1:
+                    output = first_input + second_input
+                elif opcode == 2:
+                    output = first_input * second_input
+                elif opcode == 3:
+                    print('Opcode 3 halting!')
+                    exit(3)
 
-        if opcode == 1:
-            output = first_input + second_input
-        elif opcode == 2:
-            output = first_input * second_input
-        elif opcode == 3:
-            print('Opcode 3 halting!')
-            exit(3)
-        elif opcode == 99:
-            print('Opcode 99 halting!')
-            print(input_list)
-            exit(99)
+                input_list[output_position] = output
+                pointer += 4
 
-        input_list[output_position] = output
-        pointer += 4
-
-    print(input_list)
-    print(input_list[0])
+            if input_list[0] == 19690720:
+                print(input_list)
+                print('Noun: ' + str(input_list[1]))
+                print('Verb: ' + str(input_list[2]))
+                print('Output :' + str(input_list[0]))
+                print('Answer 100+ noun + verb = ' + str((100 * input_list[1]) + input_list[2]))
 
 
 def get_instructions(input_list, pointer):
@@ -37,6 +36,13 @@ def get_instructions(input_list, pointer):
     second_input = input_list[input_list[pointer + 2]]
     return opcode, first_input, output_position, second_input
 
+
+def reset_memory():
+    reader = open('input.txt', 'r')
+    input_list = list(map(int, reader.read().split(',')))
+    reader.close()
+    pointer = 0
+    return input_list, pointer
 
 def restore_1202_program_alarm_state(input_list):
     input_list[1] = 12
