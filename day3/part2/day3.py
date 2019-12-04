08f8b51cf3e70a02c171b386e0956e79c5b9e215
pstreff <streff@convidera.com>
2019-12-04 10:42:31 +0100

    day3 part2

---
+++ day3/part2/day3.py
@@ -0,0 +1,83 @@
+
+def main():
+    points_visited = {}
+
+    reader = open('wire1.txt', 'r')
+    wire1_path = list(reader.read().split(','))
+    reader.close()
+    reader = open('wire2.txt', 'r')
+    wire2_path = list(reader.read().split(','))
+    reader.close()
+
+    # wire1_path = ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72']
+    # wire2_path = ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83']
+
+    x = y = steps = 0
+    for instr in wire1_path:
+        dx = 0
+        dy = 0
+        direction = instr[0]
+        distance = int(instr[1:])
+        if direction == 'U':
+            dy += 1
+            dx += 0
+        elif direction == 'D':
+            dy -= 1
+            dx += 0
+        elif direction == 'R':
+            dy += 0
+            dx += 1
+        elif direction == 'L':
+            dy += 0
+            dx -= 1
+        for _ in range(distance):
+            x += dx
+            y += dy
+            steps += 1
+            points_visited[(x, y)] = {'wire': 1, 'steps_wire1': steps, 'steps_wire2': 0}
+
+    x = y = steps = 0
+    for instr in wire2_path:
+        dx = 0
+        dy = 0
+        direction = instr[0]
+        distance = int(instr[1:])
+        if direction == 'U':
+            dy += 1
+            dx += 0
+        elif direction == 'D':
+            dy -= 1
+            dx += 0
+        elif direction == 'R':
+            dy += 0
+            dx += 1
+        elif direction == 'L':
+            dy += 0
+            dx -= 1
+        for _ in range(distance):
+            x += dx
+            y += dy
+            steps += 1
+            if (x, y) in points_visited and points_visited[(x, y)]['wire'] == 1:
+                points_visited[(x, y)]['wire'] = 3
+                points_visited[(x, y)]['steps_wire2'] += steps
+
+    distances = []
+    combined_steps = []
+    for (x, y) in points_visited:
+        if points_visited.get((x, y)).get('wire') == 3:
+            distance = abs(0 - x) + abs(0 - y)
+            distances.append(distance)
+            steps1 = points_visited.get((x, y)).get('steps_wire1')
+            steps2 = points_visited.get((x, y)).get('steps_wire2')
+            combined_steps.append(steps1 + steps2)
+
+    distances.sort()
+    combined_steps.sort()
+
+    print('Shortest distance: ' + str(distances[0]))
+    print('Shortest steps: ' + str(combined_steps[0]))
+
+
+if __name__ == '__main__':
+    main()
